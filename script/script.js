923105bdc3a783d0cec6a004ffe02be17b9331d4
YangdongYe <yangdong.y@husky.neu.edu>
2015-03-26 13:46:43 -0400

    20150326

--- script/script.js
+++ script/script.js
@@ -1,12 +1,7 @@
-//Assignment 3
-//Due Thursday March 26
-
-var margin = {t:100,r:100,b:200,l:150},
+var margin = {t:50,r:50,b:100,l:75},
     width = $('.canvas').width() - margin.l - margin.r,
     height = $('.canvas').height() - margin.t - margin.b;
 
-
-//Set up SVG drawing elements -- already done
 var svg = d3.select('.canvas')
     .append('svg')
     .attr('width', width + margin.l + margin.r)
@@ -14,20 +9,18 @@ var svg = d3.select('.canvas')
     .append('g')
     .attr('transform','translate('+margin.l+','+margin.t+')');
 
-//Scales
 var scales = {};
     scales.x = d3.scale.log().range([0,width]);
     scales.y = d3.scale.linear().range([height,0]);
 
-
-//Global variables
 var yVariable = "CO2 emissions (kt)",
     y0 = 1990,
-    y1 = 2013;
+    y1 = 2000;
 
-
-//d3.map for metadata
 var metaDataMap = d3.map();
+var colorScale = d3.scale.ordinal()
+    .domain(['Europe & Central Asia','South Asia','Middle East & North Africa','North Americas','Sub-Saharan Africa','East Asia & Pacific','Latin America & Caribbean'])
+    .range(['rgb(220,92,92)','rgb(220,92,180)','rgb(92,220,180)','rgb(92,180,220)','rgb(180,220,92)','rgb(120,92,220)','rgb(92,120,220)']);
 
 //TODO: create a layout function for a treemap
 var treemap = d3.layout.treemap()
@@ -35,15 +28,10 @@ var treemap = d3.layout.treemap()
         return d.values;
     })
     .value(function(d){
-        return d.data.get(y0);
+        return d.data.get(1990);
     })
     .size([width,height])
-    .sticky(true)
-    .padding([5,5,5,5]);
 
-
-
-//START!
 queue()
     .defer(d3.csv, "data/00fe9052-8118-4003-b5c3-ce49dd36eac1_Data.csv",parse)
     .defer(d3.csv, "data/metadata.csv", parseMetaData)
@@ -51,19 +39,65 @@ queue()
 
 function dataLoaded(err, rows, metadata){
 
-    //First, combine "rows" and "metadata", so that each country is assigned to a region
+    rows.forEach(function(row){
+        row.region1 = metaDataMap.get(row.key);
+    });
+    var nest = d3.nest()
+        .key(function(d){
+            return d.region1 ;
+        });
+    var nestedData = nest.entries(rows);
+    console.log(nestedData);
 
-    //Then create hierarchy based on regions, using d3.nest()
+    var root = {
+        key:"regions",
+        values: nestedData
+    };
 
-    //Finally, perform a treemap layout on the data
-
-    //draw(root);
+    draw(root);
 }
 
 function draw(root){
-    //Append <rect> element for each node in the treemap
 
-    //Also append <text> label for each tree node that is a leaf
+var nodes = svg.selectAll('.node')
+        .data( treemap(root),function(d){return d.key;})
+        .classed('leaf',function(d){
+        return !(d.children);
+    });
+
+    var nodesEnter =nodes
+        .enter()
+        .append('g')
+        .attr('class','node')
+        .attr('transform',function(d){
+            return "translate("+d.x+','+d.y+')';
+        })
+        ;
+    nodesEnter
+        .append('rect')
+        .attr('width',function(d){return d.dx;})
+        .attr('height',function(d){return d.dy;})
+        .style('fill',function(d){
+            var continent = metaDataMap.get(d.key);
+            return colorScale(continent);
+        })
+        .style('stroke','white')
+        .style('stroke-width','.5px')
+
+    nodesEnter
+        .each(function(d){
+            if(d.dx > 50 && d.dy >20){
+                var textlabel = d3.select(this)
+                    .append('text');
+                textlabel.text(function(d){
+                        return d.key;
+                    })
+                    .attr("font-family", "serif")
+                    .attr('dx', d.dx/2)
+                    .attr('dy', d.dy/2)
+                    .attr('text-anchor','middle');
+            }
+        });
 }
 
 function parse(d){
